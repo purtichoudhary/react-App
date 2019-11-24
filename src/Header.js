bb0b5acd5e78075a6fae6779a2cdef94d5516892
purtichoudhary <47091736+purtichoudhary@users.noreply.github.com>
2019-11-24 18:03:04 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <h1>Blog Page</h1>
+                <h1>Bloging with React</h1>
             </header>
         );
     }
